37bf028d6bc760ce647b45c3fa24bff7d20d5e11
Jonathanfajardo <jonnyfajardo18@gmail.com>
2024-05-20 20:39:45 -0400

    finished defining columns in the models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -3,14 +3,31 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { on } = require('nodemon');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'products',
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
